de0399c593ee5244e5f3508af8cfeda6d04d9cc0
John A. Martinez <52513621+AlexanderM1927@users.noreply.github.com>
2024-03-13 14:27:11 -0500

    Removing nginx

--- bashfile.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-sudo ln -s /etc/nginx/conf.d/casatudor /etc/nginx/sites-enabled/casatudor
-sudo ln -s /etc/nginx/sites-enabled/casatudor /etc/nginx/sites-available/casatudor
-sudo nginx -t
-sudo systemctl reload nginx
